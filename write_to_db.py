a279443843d8c5c73bdc7d2713d3870f23763b2f
Marc Hauser <marc.hauser@bkw.ch>
2022-09-16 20:31:14 +0200

    Refactoring to class

--- write_to_db.py
+++ write_to_db.py
@@ -4,21 +4,23 @@ import dotenv
 
 dotenv.load_dotenv()
 
+class PGSQL():
+    def __init__(self):
+        host = os.getenv("POSTGRES_HOST")
+        port = os.getenv("POSTGRES_PORT")
+        username = os.getenv("POSTGRES_USER")
+        password = os.getenv("POSTGRES_PASSWORD")
+        database = os.getenv("DATABASE")
 
-host = os.getenv("POSTGRES_HOST")
-port = os.getenv("POSTGRES_PORT")
-username = os.getenv("POSTGRES_USER")
-password = os.getenv("POSTGRES_PASSWORD")
-database = os.getenv("DATABASE")
+        print(host, port, username, password, database)
 
-print(host, port, username, password, database)
-
-connection = pg.connect(database=database, user=username, password=password, host=host, port=port)
-print(connection)
-example_value = 22
-example_id = 1
-cursor = connection.cursor()
-cursor.execute("UPDATE pv_realtime SET power_kw=%s WHERE id=%s", (example_value, example_id))
-
-
-
+        self.connection = pg.connect(
+            database=database, user=username, password=password, 
+            host=host, port=port)
+        print(self.connection)
+    
+    def update_realtime_power(self, plant_id, pac_kw):
+        cursor = self.connection.cursor()
+        query = f"UPDATE pv_realtime SET power_kw={pac_kw} WHERE id={plant_id}"
+        print(query)
+        cursor.execute(query)
