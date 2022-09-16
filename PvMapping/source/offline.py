e5274d83546070e7f9c0261da4fccd153e19dad3
Nicolas Vetsch <vetschn@student.ethz.ch>
2022-09-17 01:52:14 +0200

    Added offline source thread

--- PvMapping/source/offline.py
+++ PvMapping/source/offline.py
@@ -1,8 +1,17 @@
-from PvMapping.source import SourceThread
+from PvMapping.source import SourceThread, SourceItem
+import pandas as pd
 
 
 class OfflineSourceThread(SourceThread):
     def run(self) -> None:
-        while not self._should_stop:
-            # TODO: Push SourceItems into queue
-            pass
+
+        df = pd.read_pickle("../../data/2022-08.pkl").reset_index().T
+
+        for timestamp in df.index:
+            if self._should_stop:
+                break
+            row = df.loc[timestamp].dropna()
+            sample = row.sample()
+            name, power_kw = sample.index[0], sample.values[0]
+            item = SourceItem(timestamp=timestamp, power_kw=power_kw, name=name)
+            self._queue.put(item)
