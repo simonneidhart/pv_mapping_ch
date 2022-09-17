d8fc7137bc0c3b9b968e92cfe4bf080fc3d3aee7
Simon Neidhart <simon.neidhart@imsprung.ch>
2022-09-17 14:12:02 +0200

    add daily production curve

--- hackdays_map.py
+++ hackdays_map.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
 
 app = Dash(__name__)
 
 n_frames = 1440
 
 
-def serve_layout(ev):
+def serve_layout(ev,ev_sum):
     sMBT = 'pk.eyJ1IjoiY2hyaXN0b3BoaHVuemlrZXIiLCJhIjoiY2pqc2swc253Mnd0aTN3cGJucG41dWExOSJ9.6mhBXjFCMSzNQRk8u6LTHQ'
     px.set_mapbox_access_token(sMBT)
     #fig = px.scatter_mapbox(df_pvin, lat="lat", lon="lon", zoom=6, size=len(df_pvin) * [10])
@@ -37,10 +38,26 @@ def serve_layout(ev):
         },
     )
 
+    figheight=700
+    line_figure = go.Figure(layout={"height":figheight})
+    line_figure.add_trace(go.Scatter(x=ev_sum.index, y=ev_sum['kW'], fill='tozeroy', name='Total Power [kW]'))
+
+    line_figure.update_layout(
+        xaxis_title="Timestamp (UTC)",
+        yaxis_title="kW",
+    )
+
     return html.Div(
-        dcc.Graph(
-            id='map',
-            figure=fig))
+        children=[
+            html.Div(dcc.Graph(
+                id='map',
+                figure=fig)),
+            html.Div(dcc.Graph(
+                id='sum',
+                figure=line_figure))
+            ]
+        )
+
 
 
 if __name__ == "__main__":
@@ -51,9 +68,14 @@ if __name__ == "__main__":
 
     ev['ts'] = ev.index.astype(str)
 
+    ev_sum = ev.drop(columns=['ts','lat','lon'])
+    ev_sum = ev_sum.groupby(ev_sum.index).sum()
+
+    ev_sum.head()
+
     ### TO-DO :
     # 1. Read PV power [kW] of all PV systems from database
     # 2. Write callback that continiously updates the dashboard (and reads new values from database)
 
-    app.layout = serve_layout(ev)
+    app.layout = serve_layout(ev,ev_sum)
     app.run_server(debug=True)
